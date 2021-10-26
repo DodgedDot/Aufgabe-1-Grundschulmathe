2d6c42ceadb27a48a520063d12a0683388a30d0d
Tim B <t.beuke15433@hs-ansbach.de>
2021-10-26 23:19:08 +0200

    first try to abstract

--- grundschulmathe.js
+++ grundschulmathe.js
@@ -1,31 +1,36 @@
 alert('Grundschulmathe')
 
+const randomNumber = (max) => {return Math.floor(Math.random() * (max) + 1)}
+
 rightAnswers = 0
 
-const random20 = () => {return Math.floor(Math.random() * (20) + 1)}
-
-for(i = 0; i < 5; i++){
-    const a = random20()
-    const b = random20()
-    
-    let result = prompt(`Wieviel ist ${a} + ${b}?`);
-    
-    result = parseInt(result, 10);
-    
-    if(result === a + b){
-        rightAnswers++
-        alert(`Richtig ${rightAnswers}/5`)
+const calculation = (maxA, maxB, operator) => {
+    for(i = 0; i < 5; i++){
+        const a = randomNumber(maxA)
+        const b = randomNumber(maxB)
         
-    }else{
-        alert(`Falsch ${rightAnswers}/5`)
+        let result = prompt(`Wieviel ist ${a} ${operator} ${b}?`);
+        
+        result = parseInt(result, 10);
+        
+        if(result === a $operator b){
+            rightAnswers++
+            alert(`Richtig ${rightAnswers}/5`)
+            
+        }else{
+            alert(`Falsch ${rightAnswers}/5`)
+        }
     }
+
 }
+
+
 
 if(rightAnswers > 3){
     rightAnswers = 0
     for(i = 0; i < 5; i++){
-        const a = random20()
-        const b = Math.floor(Math.random() * (a) + 1)
+        const a = randomNumber(20)
+        const b = randomNumber(a)
 
         
         let result = prompt(`Wieviel ist ${a} - ${b}?`);
