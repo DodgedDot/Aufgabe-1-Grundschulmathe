7f8c1f2eb5096d8f68ab5395d1faeb67825b4118
Tim B <t.beuke15433@hs-ansbach.de>
2021-10-26 17:33:49 +0200

    fullfills task b

--- grundschulmathe.js
+++ grundschulmathe.js
@@ -1,1 +1,14 @@
 alert('Grundschulmathe');
+
+const a = random20();
+const b = random20();
+
+let result = prompt(`Wieviel ist ${a} + ${b}?`);
+
+result = parseInt(result, 10);
+
+if(result === a + b){
+    alert('Richtig');
+}else{
+    alert('Falsch');
+}
